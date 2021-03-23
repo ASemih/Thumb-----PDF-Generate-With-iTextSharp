9b1249acc2470f1bb6d1f631adf4058b56de5d5a
ASemih <a.semihayyildiz@gmail.com>
2021-03-24 00:09:09 +0300

    initial

---
+++ GeneratePDFWithiTextSharp/GeneratePDFWithiTextSharp/Controllers/StudentController.cs
@@ -0,0 +1,44 @@
+﻿using GeneratePDFWithiTextSharp.Models;
+using GeneratePDFWithiTextSharp.Report;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GeneratePDFWithiTextSharp.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Report(Student student)
+        {
+            StudentReport studentReport = new StudentReport();
+            byte[] abytes = studentReport.PrepareReport(GetStudents());
+            return File(abytes,"application/pdf");
+        }
+
+        public List<Student> GetStudents() 
+        { 
+            
+            List<Student> students = new List<Student>();
+            Student student = new Student();
+
+            for (int i = 0; i <= 7; i++)
+            {
+                student = new Student();
+                student.Id = i;
+                student.Name = "Student " + i;
+                student.Roll = "Roll " + i;
+                students.Add(student);
+            }
+
+            return students;
+        }
+    }
+}
---
+++ GeneratePDFWithiTextSharp/GeneratePDFWithiTextSharp/Report/StudentReport.cs
@@ -0,0 +1,118 @@
+﻿using GeneratePDFWithiTextSharp.Models;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+
+namespace GeneratePDFWithiTextSharp.Report
+{
+    public class StudentReport
+    {
+        #region Declaration
+        int _totalColumn = 3;
+        Document _document;
+        Font _fontStyle;
+        PdfPTable _pdfTable = new PdfPTable(3);
+        PdfPCell _pdfPCell;
+        MemoryStream _memoryStream = new MemoryStream();
+        List<Student> _students = new List<Student>();
+        #endregion
+
+        public byte[] PrepareReport(List<Student> students)
+        {
+            _students = students;
+
+            _document = new Document(PageSize.A4, 0f, 0f, 0f, 0f);
+            _document.SetPageSize(PageSize.A4);
+            _document.SetMargins(20f, 20f, 20f, 20f);
+            _pdfTable.WidthPercentage = 100;
+            _pdfTable.HorizontalAlignment = Element.ALIGN_LEFT;
+            _fontStyle = FontFactory.GetFont("Tahoma", 8f,1);
+            PdfWriter.GetInstance(_document, _memoryStream);
+            _document.Open();
+            _pdfTable.SetWidths(new float[] { 20f, 150f, 100f });
+
+            this.ReportHeader();
+            this.ReportBody();
+
+            _pdfTable.HeaderRows = 2;
+            _document.Add(_pdfTable);
+            _document.Close();
+            return _memoryStream.ToArray();
+        }
+
+        private void ReportBody()
+        {
+            _fontStyle = FontFactory.GetFont("Tahoma", 8f, 1);
+            _pdfPCell = new PdfPCell(new Phrase("Seri Numarası", _fontStyle));
+            _pdfPCell.HorizontalAlignment = Element.ALIGN_CENTER;
+            _pdfPCell.VerticalAlignment = Element.ALIGN_MIDDLE;
+            _pdfPCell.BackgroundColor = BaseColor.LIGHT_GRAY;
+            _pdfTable.AddCell(_pdfPCell);
+
+            _pdfPCell = new PdfPCell(new Phrase("İsim", _fontStyle));
+            _pdfPCell.HorizontalAlignment = Element.ALIGN_CENTER;
+            _pdfPCell.VerticalAlignment = Element.ALIGN_MIDDLE;
+            _pdfPCell.BackgroundColor = BaseColor.LIGHT_GRAY;
+            _pdfTable.AddCell(_pdfPCell);
+
+            _pdfPCell = new PdfPCell(new Phrase("Sıra", _fontStyle));
+            _pdfPCell.HorizontalAlignment = Element.ALIGN_CENTER;
+            _pdfPCell.VerticalAlignment = Element.ALIGN_MIDDLE;
+            _pdfPCell.BackgroundColor = BaseColor.LIGHT_GRAY;
+            _pdfTable.AddCell(_pdfPCell);
+            _pdfTable.CompleteRow();
+
+            _fontStyle = FontFactory.GetFont("Tahoma", 8f, 0);
+            int serialNumber = 1;
+            foreach (Student student in _students)
+            {
+                _pdfPCell = new PdfPCell(new Phrase(serialNumber++.ToString(), _fontStyle));
+                _pdfPCell.HorizontalAlignment = Element.ALIGN_CENTER;
+                _pdfPCell.VerticalAlignment = Element.ALIGN_MIDDLE;
+                _pdfPCell.BackgroundColor = BaseColor.WHITE;
+                _pdfTable.AddCell(_pdfPCell);
+
+                _pdfPCell = new PdfPCell(new Phrase(student.Name, _fontStyle));
+                _pdfPCell.HorizontalAlignment = Element.ALIGN_CENTER;
+                _pdfPCell.VerticalAlignment = Element.ALIGN_MIDDLE;
+                _pdfPCell.BackgroundColor = BaseColor.WHITE;
+                _pdfTable.AddCell(_pdfPCell);
+
+                _pdfPCell = new PdfPCell(new Phrase(student.Roll, _fontStyle));
+                _pdfPCell.HorizontalAlignment = Element.ALIGN_CENTER;
+                _pdfPCell.VerticalAlignment = Element.ALIGN_MIDDLE;
+                _pdfPCell.BackgroundColor = BaseColor.WHITE;
+                _pdfTable.AddCell(_pdfPCell);
+                _pdfTable.CompleteRow();
+            }
+
+        }
+
+        private void ReportHeader()
+        {
+            _fontStyle = FontFactory.GetFont("Tahoma",11f,1);
+            _pdfPCell = new PdfPCell(new Phrase("Sakarya Üniversitesi", _fontStyle));
+            _pdfPCell.Colspan = _totalColumn;
+            _pdfPCell.HorizontalAlignment = Element.ALIGN_CENTER;
+            _pdfPCell.Border = 0;
+            _pdfPCell.BackgroundColor = BaseColor.WHITE;
+            _pdfPCell.ExtraParagraphSpace = 0;
+            _pdfTable.AddCell(_pdfPCell);
+            _pdfTable.CompleteRow();
+
+            _fontStyle = FontFactory.GetFont("Tahoma", 9f, 1);
+            _pdfPCell = new PdfPCell(new Phrase("Öğrenci Listesi", _fontStyle));
+            _pdfPCell.Colspan = _totalColumn;
+            _pdfPCell.HorizontalAlignment = Element.ALIGN_CENTER;
+            _pdfPCell.Border = 0;
+            _pdfPCell.BackgroundColor = BaseColor.WHITE;
+            _pdfPCell.ExtraParagraphSpace = 0;
+            _pdfTable.AddCell(_pdfPCell);
+            _pdfTable.CompleteRow();
+        }
+    }
+}
